701d705d69d2876b4a802b4afd0d466b69423da9
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-09-21 13:08:14 +0800

    Add Patuti movements and bullets

--- script.js
+++ script.js
@@ -3,45 +3,144 @@ var app = new Vue({
   data() {
     return {
       images: [
-        ["moves/idle-1.png", "moves/idle-2.png"],
         [
-          "moves/left-1.png",
-          "moves/left-2.png",
-          "moves/left-3.png",
-          "moves/left-4.png",
-          "moves/left-5.png",
+          {
+            name: "moves/idle-1.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/idle-2.png",
+            height: 161.5,
+            width: 150,
+          },
         ],
         [
-          "moves/right-1.png",
-          "moves/right-2.png",
-          "moves/right-3.png",
-          "moves/right-4.png",
-          "moves/right-5.png",
+          {
+            name: "moves/left-1.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/left-2.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/left-3.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/left-4.png",
+            height: 156,
+            width: 150,
+          },
+          {
+            name: "moves/left-5.png",
+            height: 161.5,
+            width: 150,
+          },
         ],
         [
-          "moves/dock-1.png",
-          "moves/dock-2.png",
-          "moves/dock-3.png",
-          "moves/dock-4.png",
-          "moves/dock-5.png",
+          {
+            name: "moves/right-1.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/right-2.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/right-3.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/right-4.png",
+            height: 156,
+            width: 150,
+          },
+          {
+            name: "moves/right-5.png",
+            height: 161.5,
+            width: 150,
+          },
         ],
         [
-          "moves/jump-1.png",
-          "moves/jump-2.png",
-          "moves/jump-3.png",
-          "moves/jump-4.png",
-          "moves/jump-5.png",
-          "moves/jump-4.png",
-          "moves/jump-5.png",
+          {
+            name: "moves/dock-1.png",
+            width: 150,
+            height: 161.5,
+          },
+          {
+            name: "moves/dock-2.png",
+            width: 150,
+            height: 149.5,
+          },
+          {
+            name: "moves/dock-3.png",
+            width: 150,
+            height: 135,
+          },
+          {
+            name: "moves/dock-4.png",
+            width: 136,
+            height: 94.5,
+          },
+          {
+            name: "moves/dock-5.png",
+            width: 134,
+            height: 120,
+          },
+        ],
+        [
+          {
+            name: "moves/jump-1.png",
+            height: 161.5,
+            width: 150,
+          },
+          {
+            name: "moves/jump-2.png",
+            height: 157,
+            width: 154,
+          },
+          {
+            name: "moves/jump-3.png",
+            height: 150.5,
+            width: 150,
+          },
+          {
+            name: "moves/jump-4.png",
+            height: 178,
+            width: 128,
+          },
+          {
+            name: "moves/jump-5.png",
+            height: 178,
+            width: 128,
+          },
+          {
+            name: "moves/jump-6.png",
+            height: 157,
+            width: 154,
+          },
+          {
+            name: "moves/jump-7.png",
+            height: 150.5,
+            width: 150,
+          },
         ],
       ],
       currentImage: {
         image: "moves/idle-1.png",
-        width: 300,
-        height: 323,
+        height: 161.5,
+        width: 150,
+        bottom: 0,
+        left: 72.5,
       },
-      defaultWidth: 300,
-      defaultHeight: 323,
       currentMove1: 0,
       currentMove2: 0,
       speed: 200,
@@ -82,18 +181,34 @@ var app = new Vue({
     },
     move() {
       setInterval(() => {
+        console.log("-----------------------------");
+
         this.currentImage.image =
-          this.images[this.currentMove1][this.currentMove2];
+          this.images[this.currentMove1][this.currentMove2].name;
+
+        this.currentImage.height =
+          this.images[this.currentMove1][this.currentMove2].height;
+
+        this.currentImage.width =
+          this.images[this.currentMove1][this.currentMove2].width;
         this.currentMove2 =
           (this.currentMove2 + 1) % this.images[this.currentMove1].length;
 
         switch (this.currentMove1) {
           // left
           case 1:
+            if (this.currentImage.left != -47.5)
+              if (this.currentMove2 != 1) this.currentImage.left -= 10;
+
+            console.log(this.currentImage.left);
             break;
 
           // right
           case 2:
+            if (this.currentImage.left != 192.5)
+              if (this.currentMove2 != 1) this.currentImage.left += 10;
+
+            console.log(this.currentImage.left);
             break;
 
           // dock
@@ -102,12 +217,24 @@ var app = new Vue({
 
           // jump
           case 4:
+            if (this.currentMove2 > 3 && this.currentMove2 < 7) {
+              this.speed = 800;
+              this.currentImage.bottom += 50;
+            } else {
+              this.speed = 200;
+              this.currentImage.bottom = 0;
+            }
             break;
         }
-        if (this.currentMove2 === 0) this.currentMove1 = 0;
-        console.log("-----------------------------");
+
+        if (this.currentMove2 === 0) {
+          this.currentMove1 = 0;
+          this.speed = 200;
+        }
+
         console.log("move 1: " + this.currentMove1);
         console.log("move 2: " + this.currentMove2);
+        console.log("move 2: " + this.currentImage.image);
       }, this.speed);
     },
   },
